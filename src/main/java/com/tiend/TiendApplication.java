b780293792b380106e985325178727d9b396c431
DouglasVulcano <vulcanodouglas1708@gmail.com>
2024-12-01 08:36:45 -0300

    first commit

---
+++ src/main/java/com/tiend/TiendApplication.java
@@ -0,0 +1,13 @@
+package com.tiend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiend/TiendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
